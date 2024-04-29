2a87f6269018533892b32c20ad587e72c751c753
cogman1 <abdu508098@gmail.com>
2024-04-28 19:09:22 -0700

    i add condtional practice

---
+++ condtional.js
@@ -0,0 +1,8 @@
+if (22 < 18) {
+  gretting = "good day";
+} else if (22 < 20) {
+  gretting = "goodevening";
+} else {
+  gretting = "good night";
+}
+document.getElementById("condtion").innerHTML = gretting;
